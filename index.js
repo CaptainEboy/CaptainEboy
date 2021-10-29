9a91baad9c2a98f97d27ca4fdaf9e7af5c26d2b7
CaptainEboy <63905637+CaptainEboy@users.noreply.github.com>
2021-10-29 17:28:22 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+// Created by Captain Eboy
+
+
+
+var tag, nav;
+
+
+onload = function(){
+
+    
+    
+    tag = document.getElementById("tag");
+    nav = document.getElementById("name");
+    body = document.getElementById("body");
+    
+    AOS.init();
+    
+    
+    setTimeout(function() {
+        body.style.overflowY = "auto";
+        body.style.overflowX = "hidden"
+        nav.style.transition = "0.6s";
+        nav.style.height = "10vh";
+        
+        
+        
+    },00)
+    
+    
+    
+    // setTimeout(changeTag, 1000)
+    
+    // setInterval(function(){
+    //     changeTag()
+        
+    // }, 4000)
+
+}
+
+
+
+
+
